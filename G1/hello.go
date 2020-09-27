44ac6c7d9549dda39410b3121283453084e8bd7f
Kartik Ramesh <kartikx2000@gmail.com>
2020-09-27 10:01:02 +0530

    Use const prefix, instead of constructing String Literal on every call.

--- G1/hello.go
+++ G1/hello.go
@@ -7,6 +7,8 @@ import "fmt"
  * ones. Hence, we separate fmt.Println and the String.
  */
 
+const englishHelloPrefix = "Hello "
+
 // Hello returns the String to be printed.
 func Hello(name string) string {
 	return "Hello " + name + "!"
