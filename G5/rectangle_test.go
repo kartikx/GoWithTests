13925d9642cda39d877282ec5e483fe43ad62522
Kartik Ramesh <kartikx2000@gmail.com>
2020-09-28 14:26:28 +0530

    Refactor test to include checkArea() and remove redundant code.

--- G5/rectangle_test.go
+++ G5/rectangle_test.go
@@ -15,26 +15,24 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	t.Run("Testing Rectangle Area: ", func (t *testing.T) {
-		rectangle := Rectangle{width: 10.0, height: 10.0}
-
-		got := rectangle.Area()
-		expected := 100.0
-
-		if got != expected {
-			t.Errorf("Expected %.2f, Got %.2f", expected, got)
-		}
-	})
-
-	t.Run("Testing Circle Area: ", func (t *testing.T) {
-		circle := Circle{radius: 10.0}
-
-
-		got := circle.Area()
-		expected := 314.1592653589793
+	checkArea := func (t *testing.T, shape Shape, expected float64) {
+		t.Helper()
+		got := shape.Area()
 
 		if got != expected {
 			t.Errorf("Expected %g, Got %g", expected, got)
 		}
+	}
+
+	t.Run("Testing Rectangle Area: ", func (t *testing.T) {
+		rectangle := Rectangle{width: 10.0, height: 10.0}
+		
+		checkArea(t, rectangle, 100.0)
+	})
+
+	t.Run("Testing Circle Area: ", func (t *testing.T) {
+		circle := Circle{radius: 10.0}
+		
+		checkArea(t, circle, 314.1592653589793) 
 	})
 }
